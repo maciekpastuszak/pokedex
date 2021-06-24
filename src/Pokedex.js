555ef7915a3f8607216ef9c05a7cae5607483241
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-06-24 09:47:08 +0200

    update app.js with Pokedex component

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Pokecard from './Pokecard'
 
 export default class Pokedex extends Component {
     static defaultProps = {
@@ -15,8 +16,11 @@ export default class Pokedex extends Component {
     }
     render() {
         return (
-            <div>
-                
+            <div className="Pokedex">
+                <h1>Pokedex!</h1>
+               {this.props.pokemon.map((p) => (
+                    <Pokecard id={p.id} name={p.name} type ={p.type} exp={p.base} />
+                ))}
             </div>
         )
     }
